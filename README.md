293c2d2e59178f44cb6384161724db22417237c6
Rain3njoyer <shagylloooll@gmail.com>
2025-11-05 00:07:54 +0500

    Updated App.java to use environment variables and improved logging

---
+++ README.md
@@ -0,0 +1,3 @@
+# DevOps Lab 02 - CI + Mongo Integration
+
+This repo contains a simple Java app (com.napier.devops.App) that inserts and reads a document from MongoDB.
--- src/main/java/com/napier/devops/App.java
+++ src/main/java/com/napier/devops/App.java
@@ -4,15 +4,20 @@ import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class App {
     public static void main(String[] args) {
+        Logger mongoLogger = Logger.getLogger("org.mongodb.driver");
+        mongoLogger.setLevel(Level.SEVERE);
+
         System.out.println("App starting...");
 
         try {
-            // connect to mongo container named mongo-dbserver on default port 27017
-            MongoClient mongoClient = new MongoClient("mongo-dbserver", 27017);
-
+            String host = System.getenv().getOrDefault("MONGO_HOST", "localhost");
+            int port = Integer.parseInt(System.getenv().getOrDefault("MONGO_PORT", "27000"));
+            MongoClient mongoClient = new MongoClient(host, port);
             MongoDatabase database = mongoClient.getDatabase("mydb");
             MongoCollection<Document> collection = database.getCollection("test");
 
@@ -22,7 +27,6 @@ public class App {
                     .append("result", new Document("CW", 95).append("EX", 85));
 
             collection.insertOne(doc);
-
             Document myDoc = collection.find().first();
             System.out.println("Inserted doc: " + myDoc.toJson());
 
