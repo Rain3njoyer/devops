263d63b063d59c71aee3ccfefab6bb842a8a5f9c
Rain3njoyer <shagylloooll@gmail.com>
2025-11-04 19:22:00 +0500

    Lab02: CI workflow, Mongo integration, Dockerfile update

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM eclipse-temurin:17-jre-jammy
-COPY target/classes /app/classes
 WORKDIR /app
-ENTRYPOINT ["java", "-cp", "classes", "com.napier.devops.App"]
+COPY target/devops-0.1.0.jar /app/app.jar
+ENTRYPOINT ["java", "-jar", "/app/app.jar"]
--- src/main/java/com/napier/devops/App.java
+++ src/main/java/com/napier/devops/App.java
@@ -1,7 +1,36 @@
 package com.napier.devops;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Boo yah!");
+        System.out.println("App starting...");
+
+        try {
+            // connect to mongo container named mongo-dbserver on default port 27017
+            MongoClient mongoClient = new MongoClient("mongo-dbserver", 27017);
+
+            MongoDatabase database = mongoClient.getDatabase("mydb");
+            MongoCollection<Document> collection = database.getCollection("test");
+
+            Document doc = new Document("name", "Dev Student")
+                    .append("class", "DevOps")
+                    .append("year", "2024")
+                    .append("result", new Document("CW", 95).append("EX", 85));
+
+            collection.insertOne(doc);
+
+            Document myDoc = collection.find().first();
+            System.out.println("Inserted doc: " + myDoc.toJson());
+
+            mongoClient.close();
+            System.out.println("App finished.");
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
